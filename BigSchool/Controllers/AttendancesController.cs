5585ba738cc8c46be1c11dedb7f5fc3dda59aaac
thunder30 <ngoctinh3004@gmail.com>
2021-07-09 10:32:48 +0700

    Lab05

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,42 @@
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+
+
+namespace BigSchool.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int courseId)
+        {
+
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseId))
+                return BadRequest("The Attendance already exits!");
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = userId
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- BigSchool/Models/ApplicationDbContext.cs
+++ BigSchool/Models/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace BigSchool.Models
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
-
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -20,6 +20,16 @@ namespace BigSchool.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        //Fluent API
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Course)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
